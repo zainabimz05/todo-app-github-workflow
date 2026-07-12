b7c0f0484c98cd232d72b311bd1c3392c6704d69
Zainab <zainabimz05@gmail.com>
2026-07-13 02:36:24 +0500

    feat: create basic todo app

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+
+    let task = input.value;
+
+    if (task == "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let list = document.getElementById("taskList");
+
+    let item = document.createElement("li");
+
+    item.innerHTML = task + ' <button onclick="this.parentElement.remove()">Delete</button>';
+
+    list.appendChild(item);
+
+    input.value = "";
+
+}
